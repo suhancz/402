7338dd02d435ba8587f0dd99bc5c4de7fc18764e
Akos Balla <akos@balla.cloud>
2024-05-15 21:20:50 +0200

    configurable sub-address

--- 402.py
+++ 402.py
@@ -44,8 +44,11 @@ parser.add_argument(
     "-p", "--port", action=EnvDefault, envvar='PORT', 
     help="Specify the port to serve the page on (can also be specified using PORT environment variable)")
 parser.add_argument(
-    "-d", "--dns", action=EnvDefault, envvar='DNS', 
+    "-d", "--dns", action=EnvDefault, envvar='DNS',
     help="Specify the DNS server to look up remote addresses via (can also be specified using DNS environment variable)")
+parser.add_argument(
+    "-p", "--subaddress", action=EnvDefault, envvar='SUBADDRESS',
+    help="Specify the sub-address (the part in the e-mail after the '+' sign) to use for incoming e-mails (can also be specified using SUBADDRESS environment variable)")
 parser.add_argument(
     "-s", "--style", action=EnvDefault, envvar='CSS', 
     help="Specify the CSS including <style> and </style> tags for the output HTML", default='<style>li { list-style-type: "- "; }</style>')
@@ -56,22 +59,25 @@ class SimpleServer(SimpleHTTPRequestHandler):
     Web server doing the heavy lifting
     """
     def do_GET(self):
-        ip_forward = self.headers.get("X-Forwarded-For")
-        if ip_forward:
-            ip = ip_forward.split(",")[0]
-            print("returning forwarded for ip address", ip)
-
-        elif self.headers.get("X-Real-IP"):
-            ip = self.headers.get("X-Real-IP")
-            print ("returning REAL_IP ", ip)
-
+        if args.subaddress:
+            subaddress = args.subaddress
         else:
-            ip = self.client_address[0]
-            print("returning remote address ", ip)
-        dnsname = dns.reversename.from_address(ip)
+            ip_forward = self.headers.get("X-Forwarded-For")
+            if ip_forward:
+                ip = ip_forward.split(",")[0]
+                print("returning forwarded for ip address", ip)
+
+            elif self.headers.get("X-Real-IP"):
+                ip = self.headers.get("X-Real-IP")
+                print ("returning REAL_IP ", ip)
+
+            else:
+                ip = self.client_address[0]
+                print("returning remote address ", ip)
+            subaddress = dns.reversename.from_address(ip)
         with open(args.cv, encoding='utf-8') as f:
             text = f.read()
-            html = args.style + markdown.markdown(re.sub(r'(<)([A-Za-z0-9._%+-]+)(@[A-Za-z0-9.-]+\.[A-Za-z]{2,})(>)', rf'[\2\3](mailto:\2+{dnsname}\3)', text))
+            html = args.style + markdown.markdown(re.sub(r'(<)([A-Za-z0-9._%+-]+)(@[A-Za-z0-9.-]+\.[A-Za-z]{2,})(>)', rf'[\2\3](mailto:\2+{subaddress}\3)', text))
         self.extensions_map = {k: v + ';charset=UTF-8' for k, v in self.extensions_map.items()}
         self.send_response(402)
         self.send_header("Content-type", "text/html; charset=utf-8")
