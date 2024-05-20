f5dc8ecf116d53edcd83e3fa42920de07fb68448
Akos Balla <akos@balla.cloud>
2024-05-20 13:03:44 +0200

    define font

--- 402.py
+++ 402.py
@@ -17,6 +17,7 @@ import re
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from multiprocessing import Process
 from urllib.parse import parse_qs, urlparse
+from urllib.request import urlretrieve
 
 import dns.reversename  # type: ignore  # pylint: disable=import-error
 import markdown  # pylint: disable=import-error
@@ -133,11 +134,16 @@ def generatePDF(content, pdf_file):
         content (string): HTML content
         pdf_file (string): path to the PDF file
     """
+    urlretrieve(
+        "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Terminus/TerminessNerdFont-Regular.ttf",
+        "TerminessNerdFont-Regular.ttf",
+    )
     pdf_options = {"encoding": "UTF-8", "page-size": "A4"}
     if os.path.isfile(pdf_file):
         os.remove(pdf_file)
     pdfkit.from_string(args.style + content, pdf_file, options=pdf_options)
-    writer = PdfWriter(clone_from=pdf_file)
+    writer = PdfWriter(clone_from=pdf_file,
+                       font="TerminessNerdFont-Regular.ttf")
     writer.create_viewer_preferences()
     writer.add_metadata(
         {
--- README.md
+++ README.md
@@ -538,4 +538,5 @@ I formatted the below Markdown input to be YAML-parsable, because why not (linte
 External sources
 ----------------
 
-[Default style sheet by Jonas D.](https://github.com/Gioni06/terminal.css)
+* [Default style sheet by Jonas D.](https://github.com/Gioni06/terminal.css)
+* [TerminessNerdFont-Regular for the PDF by Ryan L McIntyre](https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Terminus/TerminessNerdFont-Regular.ttf)
